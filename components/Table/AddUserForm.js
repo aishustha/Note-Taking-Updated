f58e4875ccd7d9b322727808f0070e6bb89c219c
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2021-02-01 21:02:10 +0545

    AddeD

--- components/Table/AddUserForm.js
+++ components/Table/AddUserForm.js
@@ -1,41 +1,43 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+
 
 const AddUserForm = (props) => {
-    const initialFormState = {id: null, name: '', usernmae: ''}
-    const [user, setUser] = useState(initialFormState)
+  const initialFormState = { id: null, name: '', username: ''}
+  const [user, setUser] = useState(initialFormState)
 
+  const handleInputChange = (event) => {
+    const {name, value} = event.target
 
-    const handleInputChange = (event) => {
-        const {name, value} =event.target
-        setUser({...user, [name]: value})
-    }
+    setUser({ ...user, [name]: value})
+  }
   return (
     <form
-    onSubmit={(event) => {
-      event.preventDefault()
-      if (!user.name || !user.username) return
+      onSubmit = {event => {
+        event.preventDefault()
+        if (!user.name || !user.username) 
 
-      props.addUser(user)
-      setUser(initialFormState)
-    }}
-  >
-    <label>Name</label>
-    <input
-      type="text"
-      name="name"
-      value={user.name}
-      onChange={handleInputChange}
-    />
-    <label>Username</label>
-    <input
-      type="text"
-      name="username"
-      value={user.username}
-      onChange={handleInputChange}
-    />
-    <button>Add new user</button>
-  </form>
-)
+        return  
+          props.addUser(user)
+          setUser(initialFormState)
+      }} 
+    >
+      <label>Name</label>
+      <input 
+        type="text" 
+        name="name" 
+        value={user.name} 
+        onChange={handleInputChange}
+        />
+      <label>Username</label>
+      <input 
+        type="text" 
+        name="username" 
+        value={user.username}
+        onChange={handleInputChange}
+        />
+      <button>Add new user</button>
+    </form>
+  )
 }
 
-export default AddUserForm
+export default AddUserForm;
--- components/Table/Table.js
+++ components/Table/Table.js
@@ -1,34 +1,72 @@
 import React, {useState} from 'react'
-import styles from './Table.module.scss'
 import UserTable from './UserTable'
+import styles from './Table.module.scss'
 import AddUserForm from './AddUserForm'
-
+import EditUserForm from './EditUserForm'
 
 const Table = () => {
+
     const usersData = [
-        { id: 1, name: 'Tania', username: 'floppydiskette' },
-        { id: 2, name: 'Craig', username: 'siliconeidolon' },
-        { id: 3, name: 'Ben', username: 'benisphere' },
+        {id: 1, name: 'Aishwarya', username: 'aishu_xtha'},
+        {id: 2, name: 'Matina', username: 'mati_manandhar'},
+        {id: 3, name: 'Usang', username: 'usu_lama'}
     ]
 
+    const [ users, setUsers ] = useState(usersData)
+    const [ editing, setEditing ] = useState(false)
 
-    const [users, setUsers] = useState(usersData)
+    const initialFormState = { id: null, name: '', username: ''}
+    const [currentUser, setCurrentUser] = useState(initialFormState)
+
+
+    const deleteUser = (id) => {
+        setEditing(false)
+        setUsers(users.filter((user) => user.id !== id))
+    }
 
     const addUser = (user) => {
         user.id = users.length + 1
         setUsers([...users, user])
     }
+
+    
+    const editRow = (user) => {
+        setEditing(true)
+        setCurrentUser({ id: user.id, name: user.name, username: user.username })
+    }
+
+    const updateUser = (id, updateUser) => {
+        setEditing(false)
+        setUsers(users.map((user) => (user.id === id ? updateUser : user)))
+    }
+
     return (
         <div className={styles.tableContent}>
-            <h1>CRUD App</h1>
+            <h1>CRUD App with Hooks</h1>
             <div className={styles.tableFlex}>
                 <div className={styles.tableAdd}>
-                    <h2>Add User</h2>
-                    <AddUserForm addUser={addUser} />
+
+                    { editing ? (
+                        <div>
+                        <h2>Add User</h2>
+                        <EditUserForm
+                            setEditing = {setEditing}
+                            currentUser = {currentUser}
+                            updateUser = {updateUser}
+                        />
+                        </div>
+                    ) : (
+                        <div>
+                            <h2>Add User</h2>
+                            <AddUserForm addUser={addUser} />
+                        </div>
+
+                    )}
                 </div>
+
                 <div className={styles.tableView}>
                     <h2>View Users</h2>
-                    <UserTable users={users} />
+                    <UserTable users={users} deleteUser={deleteUser} editRow={editRow}/>
                 </div>
             </div>
         </div>
