bb021b5c99469ffc736deb5fc7ea90d1e231b10b
admin <admin@Manishs-iMac.local>
2021-02-03 17:54:47 +0545

    done

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -39,34 +39,36 @@ export default async(req, res) => {
     case 'DELETE':
       console.log("DELETE")
       let idDelete = req.body.id; //id -> note id to be deleted
-      const removeNote = NOTES.findIndex((note) => note.id !==idDelete ); //findIndex return the first element in the array
+      const removeNote = NOTES.findIndex((note) => note.id === idDelete ); //findIndex return the first element in the array is equal to id to be deleted with the item id
+      console.log(removeNote)
       NOTES.splice(removeNote, 1); //The splice () method adds/removes items to/from an array, and returns the removed item (s).
       res.statusCode = 200
+      console.log(NOTES)
       res.json({ data: NOTES });
       break;
 
 
-      case 'PUT':
-        let updateID =  req.query.id;
-        console.log(updateID);
-        const updateTitle = req.body.title;
-        const updateDescription = req.body.description;
-        //console.log("old data =>", NOTES)
-        
-        //loop over NOTES and find the note with the given updateID.
-        const newObj = { id: updateID, title: updateTitle, description: updateDescription };
-        for ( let i = 0; i < NOTES.length; i += 1) {
-          const note = NOTES[i];
-          if (note.id === updateID) {
-            //update the title and description for this note
-            note.title = newObj.title;
-            note.description = newObj.description;
-          }
+    case 'PUT':
+      let updateID =  req.query.id;
+      console.log(updateID);
+      const updateTitle = req.body.title;
+      const updateDescription = req.body.description;
+      //console.log("old data =>", NOTES)
+      
+      //loop over NOTES and find the note with the given updateID.
+      const newObj = { id: updateID, title: updateTitle, description: updateDescription };
+      for ( let i = 0; i < NOTES.length; i += 1) {
+        const note = NOTES[i];
+        if (note.id === updateID) {
+          //update the title and description for this note
+          note.title = newObj.title;
+          note.description = newObj.description;
         }
-        //console.log("new data =>", NOTES)
-        res.statusCode = 200
-        res.json({data: newObj});
-        break;
+      }
+      //console.log("new data =>", NOTES)
+      res.statusCode = 200
+      res.json({data: newObj});
+      break;
 
     default:
       break;
